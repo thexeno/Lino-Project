4abda1a04c5f14a0b44431a4974aecc837272faa
thexeno <thexeno@users.noreply.github.com>
2017-12-23 18:37:26 +0100

    Update stranger_things.ino

--- Arduino/StrangerThings/stranger_things.ino
+++ Arduino/StrangerThings/stranger_things.ino
@@ -1,4 +1,26 @@
-#include <APA102.h>
+/*
+ * USAGE:
+ * After upload, will run a default sentence on APA102 string. To upload a new one, just send a new one from the serial line. 
+ *
+ * stranger_lights.ino - StrangerThings lights control - Version 1.0
+ * Created on 12/2017
+ * Original sketch         (0.1)   by Enrico Sanino
+ * Final first issue       (1.0)   by Eddy
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #include <APA102.h>
 
